d78a97f3d4f39c8d82133fd47e2f485134ad90b3
Gio Rojas <gio.rojas15@gmail.com>
2023-11-10 16:47:38 -0800

    finished MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,15 +3,24 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<Item> extends ArrayDeque<Item> {
+    private Comparator<Item> c;
     public MaxArrayDeque(Comparator<Item> c) {
-
+        super();
+        this.c = c;
     }
 
     public Item max() {
-        return null;
+        return max(c);
     }
 
     public Item max(Comparator<Item> c) {
-        return null;
+        if(isEmpty())
+            return null;
+        int maxIndex = 0;
+        for(int i = 1; i < size(); i++) {
+            if(c.compare(get(i), get(maxIndex)) > 0)
+                maxIndex = i;
+        }
+        return get(maxIndex);
     }
 }
--- proj1/deque/TestMaxArrayDeque.java
+++ proj1/deque/TestMaxArrayDeque.java
@@ -1,23 +1,68 @@
 package deque;
 
+import net.sf.saxon.expr.Component;
 import org.junit.Test;
+
+import java.util.Comparator;
+
 import static org.junit.Assert.*;
 public class TestMaxArrayDeque {
-    @Test
-    /** Test creating a MaxArrayDeque with the given Comparator. */
-    public void constructorTest() {
+    private static class StringLengthComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            return a.length() - b.length();
+        }
+    }
 
+    private static class StringComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            int aLength = a.length();
+            int bLength = b.length();
+
+            for (int i = 0; i < Math.min(aLength, bLength); i++) {
+                int aChar = a.charAt(i);
+                int bChar = b.charAt(i);
+
+                if (aChar != bChar) {
+                    return aChar - bChar;
+                }
+            }
+
+            if (aLength != bLength) {
+                return aLength - bLength;
+            }
+            return 0;
+        }
     }
 
     @Test
     /** Test creating a MaxArrayDeque with the given Comparator. */
     public void maxTest() {
+        MaxArrayDeque<String> maxArrayDeque = new MaxArrayDeque<>(new StringLengthComparator());
 
+        maxArrayDeque.addLast("this");
+        maxArrayDeque.addLast("is");
+        maxArrayDeque.addLast("a");
+        maxArrayDeque.addLast("loooooong");
+        maxArrayDeque.addLast("string");
+        maxArrayDeque.addLast("x");
+
+        assertEquals(maxArrayDeque.max(), "loooooong");
+        assertEquals(maxArrayDeque.max(new StringComparator()), "x");
     }
 
     @Test
     /** Test creating a MaxArrayDeque with the given Comparator. */
     public void comparatorMaxTest() {
+        MaxArrayDeque<String> maxArrayDeque = new MaxArrayDeque<>(new StringLengthComparator());
 
+        maxArrayDeque.addLast("this");
+        maxArrayDeque.addLast("is");
+        maxArrayDeque.addLast("a");
+        maxArrayDeque.addLast("loooooong");
+        maxArrayDeque.addLast("string");
+
+        assertEquals(maxArrayDeque.max(), maxArrayDeque.get(3));
     }
 }
