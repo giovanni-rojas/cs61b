deedecf525d848c8142c6f288a78d3eea35998cf
Gio Rojas <gio.rojas15@gmail.com>
2023-12-04 23:17:46 -0800

    hw2 skeleton added

---
+++ hw2/hw2/Percolation.java
@@ -0,0 +1,25 @@
+package hw2;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+        public Percolation(int N) {             // create N-by-N grid, with all sites initially blockedr
+
+        }
+        public void open(int row, int col) {      // open the site (row, col) if it is not open already
+        }
+        public boolean isOpen(int row, int col) {  // is the site (row, col) open?
+            return false;
+        }
+        public boolean isFull(int row, int col) {  // is the site (row, col) full?
+            return false;
+        }
+        public int numberOfOpenSites() {          // number of open sites
+            return 0;
+        }
+        public boolean percolates() {             // does the system percolate?
+            return false;
+        }
+        public static void main(String[] args) {  // use for unit testing (not required, but keep this here for the autograder)
+        }
+}
