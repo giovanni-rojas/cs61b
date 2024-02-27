9d6f0dd7e630297008c9dc447582f29867b23b58
Gio Rojas <gio.rojas15@gmail.com>
2024-02-26 22:06:38 -0800

    fixed proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -6,46 +6,52 @@ import edu.princeton.cs.introcs.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
 
+import java.util.ArrayDeque;
 import java.util.Optional;
 
 public class TestArrayDequeEC {
     @Test
     public void randomizedTest() {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
         String methodCalls = "";
 
-        for (int i = 0; i < 50; i += 1) {
+        for (int i = 0; i < 100; i += 1) {
             int methodCase = StdRandom.uniform(4);
             switch (methodCase) {
                 case 0:
                     methodCalls += "addFirst(" + i + ")" + '\n';
                     sad.addFirst(i);
-                    assertEquals(methodCalls, (Integer) sad.get(0), (Integer) i);
+                    ad.addFirst(i);
+                    assertEquals(methodCalls, (Integer) sad.get(0), (Integer) ad.get(0));
                     break;
                 case 1:
                     methodCalls += "addLast(" + i + ")" + '\n';
                     sad.addLast(i);
-                    assertEquals(methodCalls, (Integer) sad.get(sad.size() - 1), (Integer) i);
+                    ad.addLast(i);
+                    assertEquals(methodCalls, (Integer) sad.get(sad.size() - 1), (Integer) ad.get(sad.size() - 1));
                     break;
                 case 2:
                     if (sad.isEmpty()) {
                         break;
                     }
                     methodCalls += "removeFirst()" + '\n';
-                    Integer first = sad.get(0);
-                    Integer removedFirst = sad.removeFirst();
-                    assertEquals(methodCalls, first, removedFirst);
+                    Integer sadFirst = sad.removeFirst();
+                    Integer adFirst = ad.removeFirst();
+                    assertEquals(methodCalls, sadFirst, adFirst);
                     break;
                 case 3:
                     if (sad.isEmpty()) {
                         break;
                     }
                     methodCalls += "removeLast()" + '\n';
-                    Integer last = sad.get(sad.size() - 1);
-                    Integer removedLast = sad.removeLast();
-                    assertEquals(methodCalls, last, removedLast);
+                    Integer sadLast = sad.removeLast();
+                    Integer adLast = ad.removeLast();
+                    assertEquals(methodCalls, sadLast, adLast);
                     break;
             }
+            methodCalls += "size()" + '\n';
+            assertEquals(methodCalls, sad.size(), ad.size());
         }
     }
 }
